26d690a378dfef4cba2349b9ab0aecf21693b824
EktoSAn <san@ektocomms.space>
2024-03-05 16:56:08 -0300

    tests: refactor servers into pytest fixtures

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,84 +1,19 @@
 import logging
-import queue
-import socket
-import time
 import threading
 from unittest.mock import patch
 
 import uvicorn
 import httpx
 import web3
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-from starlette.routing import Route
 
 import proxy
+from utils import PROXY_URL, fake_upstream, proxy_server
 
 logger = logging.getLogger()
 
-PROXY_PORT = 8000
-PROXY_URL = f"http://127.0.0.1:{PROXY_PORT}/"
-FAKE_NODE_PORT = 8888
-FAKE_NODE_URL = f"http://127.0.0.1:{FAKE_NODE_PORT}/"
-
-_fake_data = queue.Queue()
-
-fake_upstream_node = proxy.UpstreamNode(FAKE_NODE_URL)
-
-
-def get_next_fake_data():
-    return _fake_data.get(block=False)
-
-
-async def fake_node_root(request):
-    data = await request.json()
-    upstream_data, status_code = get_next_fake_data()
-    logger.debug(f"Fake node: {upstream_data}, {status_code}")
-    return JSONResponse(content=upstream_data, status_code=status_code)
-
-
-routes = [
-    Route("/", endpoint=fake_node_root, methods=["POST"]),
-]
-
-fake_node = Starlette(routes=routes)
-
-
-def run_proxy():
-    uvicorn.run(proxy.app, port=PROXY_PORT)
-
-
-def run_fake_node():
-    uvicorn.run(fake_node, port=FAKE_NODE_PORT)
-
-
-def wait_for_port(port, host='localhost', timeout=5.0):
-    start_time = time.time()
-    while True:
-        try:
-            s = socket.create_connection((host, port), timeout=timeout)
-            s.close()
-            return
-        except socket.error:
-            time.sleep(0.05)
-            if time.time() - start_time >= timeout:
-                raise socket.error("Timeout waiting for port")
-
-
-# TODO: move to fixture
-proxy_thread = threading.Thread(target=run_proxy)
-proxy_thread.daemon = True
-proxy_thread.start()
-
-fake_node_thread = threading.Thread(target=run_fake_node)
-fake_node_thread.daemon = True
-fake_node_thread.start()
-wait_for_port(PROXY_PORT)
-wait_for_port(FAKE_NODE_PORT)
-
 proxy.cache_enable(False)
 
-def test_get_balance():
+def test_get_balance_real_upstream(proxy_server):
     w3 = web3.Web3(web3.HTTPProvider(PROXY_URL + "ethereum"))
 
     # {'jsonrpc': '2.0', 'method': 'eth_getBalance', 'params': ['0x6CF63938f2CD5DFEBbDE0010bb640ed7Fa679693', '0x1272617'], 'id': 1}
@@ -86,17 +21,19 @@ def test_get_balance():
     assert balance == 10000000000000000
 
 
-def test_chain_id():
+def test_chain_id_real_upstream(proxy_server):
     w3 = web3.Web3(web3.HTTPProvider(PROXY_URL + "ethereum"))
     assert w3.eth.chain_id == 1
 
 
-def test_with_fake_node():
-    with patch.object(proxy, "get_upstream_node_for_blockchain", lambda b: fake_upstream_node):
+def test_get_balance(proxy_server, fake_upstream):
+    with patch.object(proxy, "get_upstream_node_for_blockchain", lambda b: fake_upstream.node):
         w3 = web3.Web3(web3.HTTPProvider(PROXY_URL + "ethereum"))
 
-        _fake_data.put(({'jsonrpc': '2.0', 'id': 1, 'result': '0x1'}, 200))
-        _fake_data.put(({'jsonrpc': '2.0', 'id': 1, 'result': '0x2386f26fc10000'}, 200))
+        fake_upstream.add_responses([
+            ({'jsonrpc': '2.0', 'id': 1, 'result': '0x1'}, 200),
+            ({'jsonrpc': '2.0', 'id': 1, 'result': '0x2386f26fc10000'}, 200),
+        ])
 
         assert w3.eth.chain_id == 1
 
@@ -104,10 +41,11 @@ def test_with_fake_node():
         assert balance == 10000000000000000
 
 
-def test_with_fake_node_500_error():
-    assert _fake_data.empty()
-    with patch.object(proxy, "get_upstream_node_for_blockchain", lambda b: fake_upstream_node):
-        _fake_data.put(({'jsonrpc': '2.0', 'id': 1, 'error': {"code": -32003, "message": "Transaction rejected"}}, 500))
+def test_with_fake_node_500_error(proxy_server, fake_upstream):
+    with patch.object(proxy, "get_upstream_node_for_blockchain", lambda b: fake_upstream.node):
+        fake_upstream.add_responses([
+            ({'jsonrpc': '2.0', 'id': 1, 'error': {"code": -32003, "message": "Transaction rejected"}}, 500)
+        ])
 
         response = httpx.post(PROXY_URL + "ethereum",
                               json={'jsonrpc': '2.0', 'method': 'eth_getBalance',
---
+++ tests/utils.py
@@ -0,0 +1,75 @@
+import contextlib
+import logging
+import queue
+import threading
+import time
+
+import pytest
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+import uvicorn
+
+import proxy
+
+logger = logging.getLogger()
+
+PROXY_PORT = 8000
+PROXY_URL = f"http://127.0.0.1:{PROXY_PORT}/"
+FAKE_UPSTREAM_NODE_PORT = 8888
+FAKE_UPSTREAM_NODE_URL = f"http://127.0.0.1:{FAKE_UPSTREAM_NODE_PORT}/"
+
+
+class UvicornThreadedServer(uvicorn.Server):
+    def install_signal_handlers(self):
+        pass
+
+    @contextlib.contextmanager
+    def run_in_thread(self):
+        thread = threading.Thread(target=self.run)
+        thread.start()
+        try:
+            while not self.started:
+                time.sleep(0.01)
+            yield
+        finally:
+            self.should_exit = True
+            thread.join()
+
+
+class FakeUpstreamNode:
+    def __init__(self, port, log_level='info'):
+        self.fake_data_q = queue.Queue()
+        routes = [
+            Route("/", endpoint=self.fake_node_root, methods=["POST"]),
+        ]
+        self.app = Starlette(routes=routes)
+        config = uvicorn.Config(app=self.app, host="127.0.0.1", port=port, log_level=log_level)
+        self.server = UvicornThreadedServer(config=config)
+        self.node = proxy.UpstreamNode(FAKE_UPSTREAM_NODE_URL)
+
+    async def fake_node_root(self, request):
+        data = await request.json()
+        upstream_data, status_code = self.fake_data_q.get(block=False)
+        logger.debug(f"Fake node: {upstream_data}, {status_code}")
+        return JSONResponse(content=upstream_data, status_code=status_code)
+
+    def add_responses(self, responses: list):
+        for response in responses:
+            self.fake_data_q.put(response)
+
+
+@pytest.fixture(scope="session")
+def proxy_server():
+    config = uvicorn.Config(app=proxy.app, host="127.0.0.1", port=PROXY_PORT)
+    server = UvicornThreadedServer(config=config)
+    with server.run_in_thread():
+        yield
+
+
+@pytest.fixture(scope="session")
+def fake_upstream():
+    fake_upstream_node = FakeUpstreamNode(FAKE_UPSTREAM_NODE_PORT)
+    with fake_upstream_node.server.run_in_thread():
+        fake_upstream_node.fake_data_q.queue.clear()
+        yield fake_upstream_node
